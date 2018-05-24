e2e22fbdfdaec65afbf09b23822db87ad63e16c5
MadMonk <marcin.majewski.programista@gmail.com>
2018-05-24 18:04:43 +0200

    first commit

---
+++ Assets/scripts/Board.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+
+    public Sprite[] sprites;
+
+    Tile[] Tiles;
+
+    public GameObject TilePrefab;
+
+    public int Width = 6;
+    public int Height = 4;
+
+
+	// Use this for initialization
+	void Start () {
+
+        CreateTiles();
+        PlaceTiles();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateTiles()
+    {
+        var length = Width * Height;
+
+        Tiles = new Tile[length];
+
+        for (int i=0;i<length;i++)
+        {
+            Tiles[i] = CreateTile();
+        }
+
+    }
+
+
+    Tile CreateTile()
+    {
+        var gameObject = Instantiate(TilePrefab);
+       
+
+        var tile = gameObject.GetComponent<Tile>();
+        tile.frontFace = GetRandomSprite();
+
+        return tile;
+    }
+
+    void PlaceTiles()
+    {
+        for (int i=0;i<Width*Height;i++)
+        {
+            int x = i % Width;
+            int y = i / Width;
+
+
+            Tiles[i].transform.position += Vector3.up * y+ Vector3.right * x;
+        }
+    }
+
+
+
+    Sprite GetRandomSprite()
+    {
+        int lenght = sprites.Length;
+        int index = Random.Range(0, lenght - 1);
+        return sprites[index];
+    }
+
+}
---
+++ Assets/scripts/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public bool Active = true;
+    public Sprite frontFace;
+
+
+	void Start () {
+        transform.rotation = GetTargetRotation();
+
+        var frontObject = transform.Find("front");
+        var spriteRenderer = frontObject.transform.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = frontFace;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var targetRotation = GetTargetRotation();
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5);
+		
+	}
+
+    Quaternion GetTargetRotation()
+    {
+        var rotation = Active ? Vector3.zero : (Vector3.up*180f);
+        return Quaternion.Euler(rotation);
+    }
+}
